32981ecfb470e85900ee530c8b528b5fac3061bf
sanjana <islamsanjana368@gmail.com>
2026-07-18 22:47:32 +0600

    Area of a Circle

---
+++ Discussion-1/problem-1/CircleArea.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class CircleArea {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        double radius, area;
+
+        System.out.print("Enter radius:");
+        radius = sc.nextDouble();
+
+        area = Math.PI*radius*radius;
+        System.out.println("Area of a Circle =" +area);
+
+        sc.close();
+    }
+}
